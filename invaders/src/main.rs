ff9be14c554b1e859cf777e76d26b33fb283ae70
Yonathan Setiadi <yonathan.setiadi@sicepat.com>
2022-07-06 14:52:20 +0700

    creating game using rust

---
+++ invaders/src/main.rs
@@ -0,0 +1,160 @@
+use crossterm::{
+    cursor::{Hide, Show},
+    event::{self, Event, KeyCode},
+    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    ExecutableCommand,
+};
+use rusty_audio::Audio;
+use std::{
+    error::Error,
+    sync::mpsc::{self, Receiver},
+    time::{Duration, Instant},
+    {io, thread},
+};
+
+use invaders::{
+    frame::{self, new_frame, Drawable, Frame},
+    invaders::Invaders,
+    level::Level,
+    menu::Menu,
+    player::Player,
+    render,
+    score::Score,
+};
+
+fn render_screen(render_rx: Receiver<Frame>) {
+    let mut last_frame = frame::new_frame();
+    let mut stdout = io::stdout();
+    render::render(&mut stdout, &last_frame, &last_frame, true);
+    while let Ok(curr_frame) = render_rx.recv() {
+        render::render(&mut stdout, &last_frame, &curr_frame, false);
+        last_frame = curr_frame;
+    }
+}
+
+fn reset_game(in_menu: &mut bool, player: &mut Player, invaders: &mut Invaders) {
+    *in_menu = true;
+    *player = Player::new();
+    *invaders = Invaders::new();
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    for item in &["explode", "lose", "move", "pew", "startup", "win"] {
+        audio.add(item, &format!("{}.wav", item));
+    }
+    audio.play("startup");
+
+    // Terminal
+    let mut stdout = io::stdout();
+    terminal::enable_raw_mode()?;
+    stdout.execute(EnterAlternateScreen)?;
+    stdout.execute(Hide)?;
+
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        render_screen(render_rx);
+    });
+
+    // Game loop
+    let mut player = Player::new();
+    let mut instant = Instant::now();
+    let mut invaders = Invaders::new();
+    let mut score = Score::new();
+    let mut menu = Menu::new();
+    let mut in_menu = true;
+    let mut level = Level::new();
+
+    'gameloop: loop {
+        // Per-frame init
+        let delta = instant.elapsed();
+        instant = Instant::now();
+        let mut curr_frame = new_frame();
+
+        if in_menu {
+            // Input handlers for the menu
+            while event::poll(Duration::default())? {
+                if let Event::Key(key_event) = event::read()? {
+                    match key_event.code {
+                        KeyCode::Up => menu.change_option(true),
+                        KeyCode::Down => menu.change_option(false),
+                        KeyCode::Char(' ') | KeyCode::Enter => {
+                            if menu.selection == 0 {
+                                in_menu = false;
+                            } else {
+                                break 'gameloop;
+                            }
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            menu.draw(&mut curr_frame);
+
+            let _ = render_tx.send(curr_frame);
+            thread::sleep(Duration::from_millis(1));
+            continue;
+        }
+
+        // Input handlers for the game
+        while event::poll(Duration::default())? {
+            if let Event::Key(key_event) = event::read()? {
+                match key_event.code {
+                    KeyCode::Left | KeyCode::Char('a') => player.move_left(),
+                    KeyCode::Right | KeyCode::Char('d') => player.move_right(),
+                    KeyCode::Char(' ') | KeyCode::Enter | KeyCode::Char('k') => {
+                        if player.shoot() {
+                            audio.play("pew");
+                        }
+                    }
+                    KeyCode::Esc | KeyCode::Char('q') => {
+                        audio.play("lose");
+                        reset_game(&mut in_menu, &mut player, &mut invaders);
+                    }
+                    _ => {}
+                }
+            }
+        }
+
+        // Updates
+        player.update(delta);
+        if invaders.update(delta) {
+            audio.play("move");
+        }
+        let hits: u16 = player.detect_hits(&mut invaders);
+        if hits > 0 {
+            audio.play("explode");
+            score.add_points(hits);
+        }
+        // Draw & render
+
+        let drawables: Vec<&dyn Drawable> = vec![&player, &invaders, &score, &level];
+        for drawable in drawables {
+            drawable.draw(&mut curr_frame);
+        }
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
+
+        // Win or lose?
+        if invaders.all_killed() {
+            if level.increment_level() {
+                audio.play("win");
+                break 'gameloop;
+            }
+            invaders = Invaders::new();
+        } else if invaders.reached_bottom() {
+            audio.play("lose");
+            reset_game(&mut in_menu, &mut player, &mut invaders);
+        }
+    }
+
+    // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
+    audio.wait();
+    stdout.execute(Show)?;
+    stdout.execute(LeaveAlternateScreen)?;
+    terminal::disable_raw_mode()?;
+    Ok(())
+}
